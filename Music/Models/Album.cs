ff024bd963c2840f2614a851cae10d5063ea137d
gramnum1 <charles.harrison1@marist.edu>
2017-09-19 18:21:19 -0400

    Entity Framework Tutorial

---
+++ Music/Models/Album.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Music.Models
+{
+    public class Album
+    {
+        public int AlbumId { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class MusicContext : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+    }
+}
